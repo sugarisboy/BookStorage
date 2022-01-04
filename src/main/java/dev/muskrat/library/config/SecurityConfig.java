0d057c1cc3f283fd53f439b2155880aec393bcc3
Никита Смирнов <nsmirnov@okta-solutions.com>
2022-01-04 15:38:08 +0300

    fix cors 2

--- src/main/java/dev/muskrat/library/config/SecurityConfig.java
+++ src/main/java/dev/muskrat/library/config/SecurityConfig.java
@@ -6,10 +6,18 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
 
     @Bean
     @Primary
